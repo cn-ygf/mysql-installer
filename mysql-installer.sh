3a7fc340cd7c19e88500bfe7757a2b688654fce9
ygf <ygf@cnhonker.com>
2020-06-17 11:46:05 +0800

    a

--- mysql-installer.sh
+++ mysql-installer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum install wget -y
+yum remove maraidb* -y
 wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-client-5.7.30-1.el7.x86_64.rpm -O /tmp/mysql-community-client-5.7.30-1.el7.x86_64.rpm
 wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-common-5.7.30-1.el7.x86_64.rpm -O /tmp/mysql-community-common-5.7.30-1.el7.x86_64.rpm
 wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-libs-5.7.30-1.el7.x86_64.rpm -O /tmp/mysql-community-libs-5.7.30-1.el7.x86_64.rpm
