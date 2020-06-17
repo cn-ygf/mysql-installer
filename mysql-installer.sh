ea922501b46074d3c628dc4da87c2145dce7c4f5
ygf <ygf@cnhonker.com>
2020-06-17 11:38:13 +0800

    bug

--- mysql-installer.sh
+++ mysql-installer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 yum install wget -y
-wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-client-5.7.30-1.el7.x86_64.rpm -O /tmp/
-wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-common-5.7.30-1.el7.x86_64.rpm -O /tmp/
-wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-libs-5.7.30-1.el7.x86_64.rpm -O /tmp/
-wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-server-5.7.30-1.el7.x86_64.rpm -O /tmp/
+wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-client-5.7.30-1.el7.x86_64.rpm -O /tmp/mysql-community-client-5.7.30-1.el7.x86_64.rpm
+wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-common-5.7.30-1.el7.x86_64.rpm -O /tmp/mysql-community-common-5.7.30-1.el7.x86_64.rpm
+wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-libs-5.7.30-1.el7.x86_64.rpm -O /tmp/mysql-community-libs-5.7.30-1.el7.x86_64.rpm
+wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-server-5.7.30-1.el7.x86_64.rpm -O /tmp/mysql-community-server-5.7.30-1.el7.x86_64.rpm
 
 yum localinstall /tmp/mysql-community-common-5.7.30-1.el7.x86_64.rpm -y
 yum localinstall /tmp/mysql-community-libs-5.7.30-1.el7.x86_64.rpm -y
