7277ff3b5bf0d237b2f11a665689b71b885c91a2
ygf <ygf@cnhonker.com>
2020-06-17 11:35:28 +0800

    -y

--- mysql-installer.sh
+++ mysql-installer.sh
@@ -4,10 +4,10 @@ wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-
 wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-libs-5.7.30-1.el7.x86_64.rpm -O /tmp/
 wget https://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql57-community-el7/mysql-community-server-5.7.30-1.el7.x86_64.rpm -O /tmp/
 
-yum localinstall /tmp/mysql-community-common-5.7.30-1.el7.x86_64.rpm
-yum localinstall /tmp/mysql-community-libs-5.7.30-1.el7.x86_64.rpm
-yum localinstall /tmp/mysql-community-client-5.7.30-1.el7.x86_64.rpm
-yum localinstall /tmp/mysql-community-server-5.7.30-1.el7.x86_64.rpm
+yum localinstall /tmp/mysql-community-common-5.7.30-1.el7.x86_64.rpm -y
+yum localinstall /tmp/mysql-community-libs-5.7.30-1.el7.x86_64.rpm -y
+yum localinstall /tmp/mysql-community-client-5.7.30-1.el7.x86_64.rpm -y
+yum localinstall /tmp/mysql-community-server-5.7.30-1.el7.x86_64.rpm -y
 systemctl enable mysqld
 systemctl start mysqld
 grep "temporary password" /var/log/mysqld.log
